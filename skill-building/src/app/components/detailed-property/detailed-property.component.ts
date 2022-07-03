b7669631da1880ede3d48a724df4f60cbe0352d7
github-Hamid <h.rahimi.1953@gmail.com>
2022-07-03 17:42:15 -0400

    eight

--- skill-building/src/app/components/detailed-property/detailed-property.component.ts
+++ skill-building/src/app/components/detailed-property/detailed-property.component.ts
@@ -1,7 +1,7 @@
 import { Store } from '@ngrx/store';
 import { DataManagerService } from './../../core/services/data-manager.service';
 import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
-import {ActivatedRoute} from "@angular/router"
+import {ActivatedRoute, Router} from "@angular/router"
 
 @Component({
   selector: 'app-detailed-property',
@@ -13,18 +13,20 @@ export class DetailedPropertyComponent implements OnInit {
 property_slurp : string = "";
 property : any = {};
 slideIndex: number = 1;
+subSlideIndex : number = 4;
   constructor(private route : ActivatedRoute, private dataManager : DataManagerService,private store : Store<{detailedProperty : {address_area : string,
     address : string,
     total_offering : number,
     total_term : number,
     projected_annual_returns_min : number,
     img_src_main : string,
-    images : Array<string>}}>) { }
+    images : Array<string>}}>,
+    private router : Router) { }
 
 // showing selected slides and hiding rest of them
 showDivs(n: number) {
   var i;
-  var images = document.querySelectorAll<HTMLElement>('.main__div-image');
+  var images = document.querySelectorAll<HTMLElement>('.div-image__main-image');
   if (n > images.length) {
     this.slideIndex = 1;
   }
@@ -38,13 +40,52 @@ showDivs(n: number) {
   images[this.slideIndex - 1].style.display = 'block';
 }
 
+showDivSubImages(n : number)
+{
+  var i;
+  var images = document.querySelectorAll<HTMLElement>('.div-image__sub-image');
+  if (n > images.length) {
+    this.subSlideIndex = 4;
+  }
+  if (n < 1) {
+    this.subSlideIndex = images.length;
+  }
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+  }
+  console.log("images in func:", images);
+  images[this.subSlideIndex - 1].style.display = 'block';
+  images[this.subSlideIndex - 2].style.display = 'block';
+  images[this.subSlideIndex - 3].style.display = 'block';
+  images[this.subSlideIndex - 4].style.display = 'block';
+}
+
 //selecting next slides
 plusDivs(n: number) {
   this.showDivs((this.slideIndex += n));
 }
 
+//selecting next slides
+plusSubDivs(n: number) {
+  this.showDivSubImages((this.subSlideIndex += n));
+}
+
+
 ngAfterViewChecked(): void {
   this.showDivs(this.slideIndex);
+  this.showDivSubImages(this.subSlideIndex);
+}
+
+subImageClicked(url : string, index : number)
+{
+  console.log("url:", url, "index:", index);
+  this.slideIndex = index + 1;
+   this.showDivs(1);
+}
+
+backToProperties()
+{
+  this.router.navigateByUrl("/properties");
 }
 
   ngOnInit(): void {
